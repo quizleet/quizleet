7fbdae84ada97bc41bf2da5b97a2eda492ad7ed2
John Wage <john.m.wage@gmail.com>
2024-03-06 10:59:43 -0500

    Add getProblems middleware to save and favorite routes, respectively

--- quizleet/server/routes/api.js
+++ quizleet/server/routes/api.js
@@ -1,18 +1,23 @@
-import express from "express";
-import controller from "../controllers/controller.js"
+import express from 'express';
+import controller from '../controllers/controller.js';
 
 const router = express.Router();
 
-router.get("/", controller.getProblems, (req, res) => {
+router.get('/', controller.getProblems, (req, res) => {
   res.status(200).send(res.locals.data);
 });
 
-router.patch("/save", controller.save, (req, res) => {
-    res.sendStatus(200);
+router.patch('/save', controller.save, controller.getProblems, (req, res) => {
+  res.status(200).send(res.locals.data);
 });
 
-router.patch("/favorite", controller.favorite, (req, res) => {
-    res.sendStatus(200);
-});
+router.patch(
+  '/favorite',
+  controller.favorite,
+  controller.getProblems,
+  (req, res) => {
+    res.status(200).send(res.locals.data);
+  }
+);
 
 export default router;
